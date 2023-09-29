df5fc7c6d269c26ab5d8c1ae49f0294b447b80cc
Macbook Pro JN <jniu098@gmail.com>
2023-09-29 23:36:02 +1300

    in addition to connect to the robot and read the battery voltage, this script can read cmd_vel topic now, also able to publish message to cmd_vel topic and rbg_light topic to control the robot movement and the rgb effect for the robot. Next step will be creating a swagger file (rest api) & tidy up the code.

--- src/components/RobotConnection.jsx
+++ src/components/RobotConnection.jsx
@@ -1,53 +1,152 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import ROSLIB from 'roslib'
 
 function RobotConnection() {
     const [status, setStatus] = useState("Not connected");
-    const [voltage, setVoltage] = useState(null);
     const ros = new ROSLIB.Ros({ encoding: 'ascii' });
 
-    useEffect(() => {
-        if (status === 'Connected') {
-            readVoltageTopic();
-        }
-    }, [status]);
 
     function connect() {
-        if (status !== 'Connected') {
-            ros.connect("ws://192.168.1.11:9090")
 
-            ros.on('error', function (error) {
-                console.log(error)
-                setStatus(error.toString())
-            })
+        ros.connect("ws://192.168.0.167:9090")
 
-            ros.on('connection', function () {
-                console.log('Connected!')
-                setStatus("Connected")
-            })
+        ros.on('error', function (error) {
+            console.log(error)
+            setStatus(error.toString())
+        })
 
-            ros.on('close', function () {
-                console.log('Connection closed')
-                setStatus("Connection closed")
-            })
-        }
+        ros.on('connection', function () {
+            console.log('Connected!')
+            setStatus("Connected")
+        })
+
+        ros.on('close', function () {
+            console.log('Connection closed')
+            setStatus("Connection closed")
+        })
     }
 
     function readVoltageTopic() {
+
+        console.log("start reading")
+
         const voltageTopic = new ROSLIB.Topic({
             ros: ros,
             name: '/voltage',
             messageType: 'std_msgs/Float32', // adjust the message type
         });
 
+        console.log("topic defined")
+
         voltageTopic.subscribe(function (message) {
-            console.log("start reading")
+            console.log("voltage here")
             console.log('Received message on ' + voltageTopic.name + ': ' + message.data);
-            setVoltage(message.data);
-            // You may unsubscribe after receiving the message if you no longer need to listen to this topic.
             voltageTopic.unsubscribe();
         });
+
+        connect();
     }
+
+
+    function readCmdVelTopic() {
+
+        console.log("start reading")
+
+        const cmdVelTopic = new ROSLIB.Topic({
+            ros: ros,
+            name: '/cmd_vel',
+            messageType: 'geometry_msgs/Twist', // adjust the message type
+        });
+
+        console.log("topic defined")
+
+        cmdVelTopic.subscribe(function (message) {
+            console.log("cmd vel here")
+            console.log('Received message on ' + cmdVelTopic.name + ': ', message); // logging the entire message object
+            cmdVelTopic.unsubscribe();
+        });
+
+        connect();
+    }
+
+    function publishToRGBLight() {
+
+        const rgbLightTopic = new ROSLIB.Topic({
+            ros: ros,
+            name: '/RGBLight',
+            messageType: 'std_msgs/Int32'
+        });
+
+        const message = new ROSLIB.Message({
+            data: 0 // the data you want to publish
+        });
+
+        // Connecting to ROS
+        ros.on('connection', function () {
+            console.log('Connected to WebSocket server.');
+
+            // Publishing message once connected
+            rgbLightTopic.publish(message);
+            console.log('Published message to /RGBLight');
+        });
+
+        connect();
+
+    }
+
+
+    function publishMoveToCmdVel() {
+
+        console.log("Start publishing");
+
+        const cmdVelTopic = new ROSLIB.Topic({
+            ros: ros,
+            name: '/cmd_vel',
+            messageType: 'geometry_msgs/Twist'
+        });
+
+        const message = new ROSLIB.Message({
+            linear: {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0
+            },
+            angular: {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0
+            }
+        });
+
+        console.log("Topic and message defined");
+
+        // Connecting to ROS
+        ros.on('connection', function () {
+            console.log('Connected to WebSocket server.');
+
+            // Publishing message once connected
+            cmdVelTopic.publish(message);
+            console.log('Published message to /cmd_vel');
+        });
+
+        ros.on('error', function (error) {
+            console.log('Error connecting to WebSocket server: ', error);
+        });
+
+        ros.on('close', function () {
+            console.log('Connection to WebSocket server closed.');
+        });
+
+        // Assuming you have a 'connect' function to initiate the connection to ROS.
+        connect();
+    }
+
+
+
+
+
+
+
 
 
     return (
@@ -55,8 +154,11 @@ function RobotConnection() {
             <div>
                 {status} <span>to the robot</span>
             </div>
-            {voltage && <div>Voltage: {voltage}</div>}
             <button onClick={() => connect()}>Connect</button>
+            <button onClick={() => readVoltageTopic()}>Voltage</button>
+            <button onClick={() => readCmdVelTopic()}>Cmd Vel</button>
+            <button onClick={() => publishToRGBLight()}>RGB Light</button>
+            <button onClick={() => publishMoveToCmdVel()}>Move</button>
         </div>
     )
 }
