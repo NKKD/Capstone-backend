b34a0f32f692f5b288f2e8c2b84c782c2dfe47f4
Macbook Pro JN <jniu098@gmail.com>
2023-10-07 16:39:19 +1300

    Further improvement before user authentication

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ import cors from "cors";
 import router from './routes/robot-route.js'
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json' assert {type: 'json'};
-
-
+const redis = require('redis');
+const client = redis.createClient();
+const verifyToken = require('./middlewares/verifyTokenMiddleware');
 
 const app = express();
 app.use(cors());
 app.use('/', express.static('public'))
-app.use("/api", router);
+
+//this applies verifyToken to /api only
+app.use("/api",verifyToken, router);
 
 app.use(
     '/api-docs',
     swaggerUi.serve,
     swaggerUi.setup(swaggerDocument)
 );
+
+client.on("error", function(error) {
+    console.error("Redis Error: ", error);
+});
 
 
 app.listen(3000, function () {
