f4dd5ba90789a9b73ff886f91a6e8da8d60960d2
Macbook Pro JN <jniu098@gmail.com>
2023-10-11 15:25:51 +1300

    Update README file

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from "cors";
+import router from './routes/robot-route.js'
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' assert {type: 'json'};
+
+const app = express();
+app.use(cors());
+app.use('/', express.static('public'))
+
+//this applies verifyToken to /api only
+app.use("/api", router);
+
+app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+);
+
+
+
+app.listen(3000, function () {
+    console.log("I'm actively listening at PORT 3000.............");
+})
+
+
